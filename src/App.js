70d044f682682caa8cceb9a9bc03a2053ab743f8
camilapaleno <88165955+camilapaleno@users.noreply.github.com>
2024-01-20 12:28:42 -0800

    Added Hero Text & Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import Spline from '@splinetool/react-spline';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <div className='nav'>
+        <ul>
+          <li> <span><b>code</b> stack</span> development</li>
+          <li>about</li>
+          <li>services</li>
+          <li>contact</li>
+        </ul>
+      </div>
+      <div className='hero'>
+
+        <div className='text'>
+          <h1>We're obsessed with creating mobile applications.</h1>
+          <p>At Code Stack, our expert developers and designers tirelessly pushes boundaries to make your app vision a reality.</p>
+          <a href='#'>Reach Out to Us</a> <a href='#'>Learn More</a>
+          <br/>
+          <div>
+            <span>300+</span>
+            <p>Applications Made</p>
+          </div>
+          <div>
+            <span>20+</span>
+            <p>Years Experience</p>
+          </div>
+          <div>
+            <span>50+</span>
+            <p>Partners</p>
+          </div>
+        </div>
+
+        <div className='spline-container'>
+          <div className='spline'>
+            <Spline scene="https://prod.spline.design/iEvoloQ8wiW1jRWU/scene.splinecode" />
+          </div>
+        </div>
+      
+      </div>
     </div>
+
   );
 }
-
-export default App;
